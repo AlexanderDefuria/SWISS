86156e17b640e42c8b7be2ca8124e210b4c3945e
Alexander De Furia <defuria.alexander@gmail.com>
2019-05-02 13:48:40 -0400

    SQLite-CSV.py ready to be tested with Django SQLite Database

--- SQLite-CSV.py
+++ SQLite-CSV.py
@@ -1,12 +1,23 @@
 import sqlite3
+import sys
 
-with open('/path/to/file.csv', 'w+') as write_file:
+first_arg = sys.argv[1]
+second_arg = sys.argv[2]
+
+
+if ".csv" not in first_arg:
+	first_arg + ".csv"
+
+
+
+
+with open('first_arg', 'w+') as write_file:
 # open a file to write to
-    conn = sqlite3.connect('/path/to/database/database.db')
+    conn = sqlite3.connect('first_arg')
     # connect to your database
     cursor = conn.cursor()
     # create a cursor object (which lets you address the table results individually)
-    for row in cursor.execute('SELECT * FROM your_table_name'):
+    for row in cursor.execute('SELECT * FROM ' + second_arg):
     # use the cursor as an iterable
         write_file.write(row)
         # write to the csv, then you can open the csv in Excel.
