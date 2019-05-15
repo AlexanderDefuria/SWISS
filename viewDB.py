15cab813899e5bb1b9854b9ea850b08f3c526d46
Alexander De Furia <defuria.alexander@gmail.com>
2019-05-15 15:46:52 -0400

    Did more work on data visualization and experimentation:

--- viewDB.py
+++ viewDB.py
@@ -1,26 +1,24 @@
 import pandas as pd
 import numpy as np
+import sqlalchemy as sa
 import matplotlib.pyplot as plt
-import sqlite3
 
-conn = sqlite3.connect("db.sqlite3")
-cur = conn.cursor()
-# cur.execute("select * from entry_match where id is 47;")
+conn = sa.create_engine("sqlite:///db.sqlite3")
+conn = conn.connect()
 
-df = pd.read_sql_query("select * from entry_match where team_name is 'MaxTech';", conn)
-
-
-#results = cur.fetchall()
+df = pd.read_sql_table("entry_match", conn)
+df = df.drop(["team_name", "team_number", "id", "second_start", "first_start", "climb"], axis=1)
+df = df.sort_values(by="match_number")
+df = df["auto_cargo"]
 
 
 print(df)
 
+
 plt.close(fig='all')
 df.cumsum()
-plt.figure()
 df.plot()
 plt.show()
-
 
 
 conn.close()
