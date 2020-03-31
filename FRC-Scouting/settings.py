3272d5dafd81066849a313614e956db107f79d18
AlexanderDefuria <defuria.alexander@gmail.com>
2020-03-31 18:14:06 -0400

    Update settings.py

--- FRC-Scouting/settings.py
+++ FRC-Scouting/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
