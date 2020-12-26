ecd5adcb043946c5293420ef3f1346c8f6f55066
Nick Kerstens <34928894+NickKerstens@users.noreply.github.com>
2020-12-26 18:08:49 -0500

    Attempt to add notifications

--- static/entry/script.js
+++ static/entry/script.js
@@ -75,6 +75,40 @@ function teamFilter(field) {
 	document.getElementsByClassName("teamChip")
 }
 
+// Function that asks permission to show notifications 
+
+function askNotificationPermission() {
+  // function to actually ask the permissions
+  function handlePermission(permission) {
+    // Whatever the user answers, we make sure Chrome stores the information
+    if(!('permission' in Notification)) {
+      Notification.permission = permission;
+    }
+
+    // set the button to shown or hidden, depending on what the user answers
+    if(Notification.permission === 'denied' || Notification.permission === 'default') {
+      notificationBtn.style.display = 'block';
+    } else {
+      notificationBtn.style.display = 'none';
+    }
+  }
+
+  // Let's check if the browser supports notifications
+  if (!('Notification' in window)) {
+    console.log("This browser does not support notifications.");
+  } else {
+    if(checkNotificationPromise()) {
+      Notification.requestPermission()
+      .then((permission) => {
+        handlePermission(permission);
+      })
+    } else {
+      Notification.requestPermission(function(permission) {
+        handlePermission(permission);
+      });
+    }
+  }
+}
 
 // Function that controls the team select drawer
 
