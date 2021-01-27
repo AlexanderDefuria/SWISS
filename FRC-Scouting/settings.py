48ec7f448de60693ba9b372b507d94ebbd6b0156
AlexanderDeFuria <defuria.alexander@gmail.com>
2021-01-27 01:49:03 +0000

    still in progress just need to sync

--- FRC-Scouting/settings.py
+++ FRC-Scouting/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.entry.apps.EntryConfig',
     'django_ajax',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.entry.apps.EntryConfig'
 ]
 
 MIDDLEWARE = [
