85d875f8952135b33dc5fead4d90af57b32f59e9
Nick Kerstens <34928894+NickKerstens@users.noreply.github.com>
2021-01-26 22:12:16 -0500

    Revert "Revert "Merge branch 'deploy' of https://github.com/AlexanderDefuria/FRC-Scouting into deploy""
    This reverts commit 00deaa53611e25840b357ff3b01799ad09319439.

---
+++ venv/lib/python3.8/site-packages/django_ajax/encoder.py
@@ -0,0 +1,68 @@
+"""
+Utils
+"""
+from __future__ import unicode_literals
+
+import json
+from datetime import date
+from django.http.response import HttpResponseRedirectBase, HttpResponse
+from django.template.response import TemplateResponse
+from django.utils.encoding import force_text
+from django.db.models.base import ModelBase
+from decimal import Decimal
+
+
+class LazyJSONEncoderMixin(object):
+    """
+    A JSONEncoder subclass that handle querysets and models objects.
+    Add how handle your type of object here to use when dump json
+
+    """
+
+    def default(self, obj):
+        # handles HttpResponse and exception content
+        if issubclass(type(obj), HttpResponseRedirectBase):
+            return obj['Location']
+        elif issubclass(type(obj), TemplateResponse):
+            return obj.rendered_content
+        elif issubclass(type(obj), HttpResponse):
+            return obj.content
+        elif issubclass(type(obj), Exception) or isinstance(obj, bytes):
+            return force_text(obj)
+
+        # this handles querysets and other iterable types
+        try:
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+
+        # this handlers Models
+        if isinstance(obj.__class__, ModelBase):
+            return force_text(obj)
+
+        if isinstance(obj, Decimal):
+            return float(obj)
+
+        if isinstance(obj, date):
+            return obj.isoformat()
+
+        return super(LazyJSONEncoderMixin, self).default(obj)
+
+
+class LazyJSONEncoder(LazyJSONEncoderMixin, json.JSONEncoder):
+    pass
+
+
+def serialize_to_json(data, **kwargs):
+    """
+    A wrapper for simplejson.dumps with defaults as:
+
+    cls=LazyJSONEncoder
+
+    All arguments can be added via kwargs
+    """
+    kwargs['cls'] = kwargs.get('cls', LazyJSONEncoder)
+
+    return json.dumps(data, **kwargs)
