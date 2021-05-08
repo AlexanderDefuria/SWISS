826dfb11d8dd234be0368350a48ab5429cea6c8b
Alexander De Furia <defuria.alexander@gmail.com>
2021-05-08 19:24:26 -0400

    fixed urls?

--- apps/entry/urls.py
+++ apps/entry/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     path('experimental/getcsv/', views.get_csv_ajax, name='getcsv'),
     path('tutorial/', views.Tutorial.as_view(), name='tutorial'),
     path('welcome/', views.Welcome.as_view(), name='welcome'),
-	path('import/', views.Import.as_view(), name='import'),
+	path('import/', views.import_from_first, name='import'),
 
     path('logout/', views.logout, name='logout'),
     path('login/', views.login, name='login'),
