3357ccc6db282e618dd96b3a890cb528282f8bfb
Alexander De Furia <defuria.alexander@gmail.com>
2021-05-20 19:42:12 -0400

    added logging extra in home dir

--- FRC-Scouting/settings.py
+++ FRC-Scouting/settings.py
@@ -142,7 +142,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['file'],
-            'level': 'DEBUG',
+            'level': 'INFO',
             'propagate': True,
         },
     },
