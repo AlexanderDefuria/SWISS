1ffb5097096dbbc0b50c9477bcdacd60754c6059
Alexander De Furia <defuria.alexander@gmail.com>
2021-05-19 22:56:19 -0400

    added spot for nick to add the discord bot in the middleware marked with a TODO tag

--- apps/entry/middleware.py
+++ apps/entry/middleware.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from apps.entry.models import TeamMember
+from django.http import HttpResponse
+from django.conf import settings
+import traceback
 
 from apps.entry.urls import urlpatterns
 from apps import config
@@ -79,3 +82,28 @@ class ValidateUser:
                 actlevel += 1
 
         return actlevel >= reqlevel
+
+
+class ErrorHandlerMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        if not settings.DEBUG:
+            if exception:
+                # Format your message here
+                message = "**{url}**\n\n{error}\n\n````{tb}````".format(
+                    url=request.build_absolute_uri(),
+                    error=repr(exception),
+                    tb=traceback.format_exc()
+                )
+                # Do now whatever with this message
+                # e.g. requests.post(<slack channel/teams channel>, data=message)
+                # TODO Put discord thingy here @Nick
+
+            return HttpResponse("Error processing the request.", status=500)
