1d5b541836559f5028ba966ff24fa7332b72e433
Nick Kerstens <nick.k@redstudiosmedia.ca>
2022-01-22 19:56:18 -0500

    Swapped View and Enter

--- apps/hours/urls.py
+++ apps/hours/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(url=reverse_lazy('promotional:index'), permanent=True)),
-    path('enter', views.ViewHours.as_view(), name='enter'),
-    path('view', views.EnterHours.as_view(), name='view'),
+    path('enter', views.EnterHours.as_view(), name='enter'),
+    path('view', views.ViewHours.as_view(), name='view'),
 ]
