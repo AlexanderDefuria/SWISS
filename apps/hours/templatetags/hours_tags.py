5816646dc9d0597bd9101e745175e2ab27e1a1e0
Alexander De Furia <defuria.alexander@gmail.com>
2022-01-24 15:41:12 -0500

    @NickKerstens this should be all g but not 100% tested yet.

--- apps/hours/templatetags/hours_tags.py
+++ apps/hours/templatetags/hours_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from hours.models import *
 
 register = template.Library()
 
@@ -6,4 +7,17 @@ register = template.Library()
 @register.filter
 def modulo(num, val):
     return str(num // val) + " " + str(num % val)
+
+
+@register.filter
+def get_total_user_hours(request):
+    logs = Log.objects.all().filter(status=1)
+    if Gremlin.objects.all().contains(user=request.user.id):
+        logs = logs.filter(gremlin__user=request.user.id)
+
+    total_minutes = 0
+    for log in logs:
+        total_minutes += log.minutes
+
+    return total_minutes
 
