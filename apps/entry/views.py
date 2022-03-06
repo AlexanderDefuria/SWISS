5ceb8bc82598e3bb26e77f44d837043e19e7d1f3
Alexander DeFuria <defuria.alexander@gmail.com>
2022-03-06 11:15:02 -0500

    Update views.py

--- apps/entry/views.py
+++ apps/entry/views.py
@@ -61,12 +61,7 @@ def match_scout_submit(request, pk):
         match.missed_balls_auto = request.POST.get('missed_balls', 0)
         match.intake_type = request.POST.get('intakeType', 0)
         match.under_defense = request.POST.get('under_defense', 0)
-        if request.POST.get('defended_by',0) == '':
-            match.defended_by = 0
-        if type(request.POST.get('defended_by')) == type("s"):
-            match.defended_by = int(request.POST.get('defended_by'), 0)
-        else:
-            match.defended_by = request.POST.get('defended_by', 0)
+        match.defended_by = 0
 
         match.offensive_fouls = request.POST.get('offensive_fouls', 0)
 
