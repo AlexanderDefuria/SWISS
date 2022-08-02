98f84aa58eefc123788c352a92cf19784d13ea48
Nick Kerstens <nick.k@redstudiosmedia.ca>
2022-08-02 19:45:50 -0400

    Schedule Search Rework

--- static/entry/script.js
+++ static/entry/script.js
@@ -109,16 +109,16 @@ function logFilter(field) {
 
 function scheduleFilter(field) {
 	let filter = document.getElementById(field).value.toString();
-	let all = document.getElementsByClassName('scheduleTile')
+	let all = document.getElementsByClassName('teamLink')
 	if (field === "matchNumber") {
 		for (let i = 0; i < all.length; i++) {
-			if (all[i].name.toString().indexOf(filter) > -1)
+			if (all[i].id.toString().indexOf(filter) > -1)
 				all[i].style.display = "";
 			else
 				all[i].style.display = "none";
 
 		}
-	} else if (field === "matchNumber") {
+	} else if (field === "teamNumber") {
 		for (let i = 0; i < all.length; i++) {
 			if (all[i].name.toString().toUpperCase().indexOf(filter.toString().toUpperCase()) > -1)
 				all[i].style.display = "";
