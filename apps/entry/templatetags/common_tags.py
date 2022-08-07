41653dd7841b3823d34490a243ed37212ccf8b46
Alexander De Furia <defuria.alexander@outlook.com>
2022-08-07 14:14:12 -0400

    team pill reusable components

--- apps/entry/templatetags/common_tags.py
+++ apps/entry/templatetags/common_tags.py
@@ -14,14 +14,43 @@ from apps.entry import views
 register = template.Library()
 
 
-@register.inclusion_tag("entry/sidebar.html", takes_context=True)
+@register.inclusion_tag("entry/components/sidebar.html", takes_context=True)
 def get_sidebar(context):
     return {'user': context['user']}
 
 
-@register.inclusion_tag("entry/topbar.html")
+@register.inclusion_tag("entry/components/topbar.html")
 def get_topbar(image):
     return {'image': image}
+
+
+@register.inclusion_tag("entry/components/pill.html")
+def get_pill(team):
+    return {'team': team}
+
+
+@register.inclusion_tag("entry/components/pill_link.html")
+def get_pill(team, next_page):
+    return {
+        'team': team,
+        'next_page': next_page
+    }
+
+
+@register.inclusion_tag("entry/components/team-link.html")
+def get_team_link(team, next_page):
+    return {
+        'team': team,
+        'next_page': next_page
+    }
+
+
+@register.inclusion_tag("entry/components/team-card-select.html")
+def get_team_card_select(team, next_page):
+    return {
+        'team': team,
+        'next_page': next_page
+    }
 
 
 @register.filter
