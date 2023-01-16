317a07aa8a096acbbac0179a3e44ebc9580b65ca
Vortex148 <95885247+Vortex148@users.noreply.github.com>
2023-01-15 20:42:08 -0500

    Fixed issue #99

--- apps/entry/views.py
+++ apps/entry/views.py
@@ -305,14 +305,12 @@ class TeamList(LoginRequiredMixin, generic.ListView):
     template_name = 'entry/teams.html'
     context_object_name = "team_list"
     model = Team
-
     def get_queryset(self):
         teams = get_present_teams(self.request.user)
         if teams.count() == 1 and teams.first() == Team.objects.first():
             return HttpResponseRedirect(reverse_lazy('entry:team_settings_not_found_error'))
 
         return teams
-
 
 class Import(LoginRequiredMixin, FormMixin, generic.TemplateView):
     login_url = 'entry:login'
@@ -579,7 +577,7 @@ class Registration(FormMixin, generic.TemplateView):
             user.first_name = form.cleaned_data['first_name']
             user.last_name = form.cleaned_data['last_name']
             user.email = form.cleaned_data['email']
-
+            print(user)
             try:
                 if form.cleaned_data['create_new_org']:
                     org = Organization()
@@ -590,14 +588,15 @@ class Registration(FormMixin, generic.TemplateView):
                     user.orgmember.organization = org
                 else:
                     org = Organization.objects.get(name=form.cleaned_data['org_name'])
-                    if org.reg_id[:6] != form.cleaned_data['org_reg_id']:
+                    if str(org.reg_id)[:6] != form.cleaned_data['org_reg_id']:
                         raise Organization.DoesNotExist
                     user.orgmember.organization = org
             except Organization.DoesNotExist:
                 form.add_error('org_name', "Org Name or UUID is incorrect.")
                 form.add_error('org_reg_id', "Org Name or UUID is incorrect.")
                 context = {'form': form}
-                return render(request, 'entry/login.html', context)
+                print('org does not exist ' + form.cleaned_data)
+                return render(request, 'entry/register.html', context)
 
             print(user)
             user.save()
