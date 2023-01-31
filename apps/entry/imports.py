62847db731113affcc8aabd3aaa83ca832d6117d
Vortex148 <95885247+Vortex148@users.noreply.github.com>
2023-01-31 14:29:02 -0500

    with import

--- apps/entry/imports.py
+++ apps/entry/imports.py
@@ -1,65 +1,16 @@
 import requests
 import json
 import os
-from apps.entry.models import *
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+params = dict(
+    teamNumber = 4343
+)
 
 
-with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
+responce = requests.get(f'https://frc-api.firstinspires.org/v3.0/2020/teams', headers={
+    'Authorization': f'Basic dm9ydGV4MTQ4OmFkY2E5ZDI5LTU3YWUtNDJiMi1hMTY3LWZjMDhiMzg2Mzg4OQ=='}, params=params).json()
 
+responce = responce['teams'][0]['nameShort']
 
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
-
-
-_headers = {'Authorization': f'Basic {get_secret("FIRST_API_BASE64")}'}
-_events = []
-_baseUrl = 'https://frc-api.firstinspires.org/v3.0/2023/'
-def getTeams(page = None, eventCode = None):
-    _request = f'{_baseUrl}teams'
-    if page and eventCode:
-        _request += f'?page={page}&eventCode={eventCode}'
-    elif eventCode:
-        _request += f'?eventCode={eventCode}'
-    elif page:
-        _request += f'?page={page}'
-    else:
-        pass
-    team_count = requests.get(_request, headers=_headers).json()
-    return team_count
-
-def getEvent():
-    event = requests.get(f'{_baseUrl}events', headers=_headers).json()
-    return event
-
-
-baseTeamInfo = requests.get(f'{_baseUrl}teams', headers=_headers).json()
-
-pageTotal = int(baseTeamInfo['pageTotal'])
-event = getEvent()['Events']
-
-def getTeamList(eventCode = None):
-    for currentPage in range(1,pageTotal):
-        pageInfo = getTeams(currentPage,eventCode)
-        getTeamNumbers = pageInfo['teamCountPage']
-        for teamIndex in range(getTeamNumbers):
-            teamInfo = pageInfo['teams'][teamIndex]
-            print(f"{teamInfo['nameShort']} {teamInfo['teamNumber']} located in {teamInfo['stateProv']}, {teamInfo['country']}")
-
-
-
-for currentEvent in range(len(event)):
-    eventInfo = event[currentEvent]
-    _events.append(eventInfo['code'])
-    print(f"{eventInfo['name']} has the first key of {eventInfo['code']}, and the district key of {eventInfo['districtCode']}, is the event type of {eventInfo['type']}, starts on {eventInfo['dateStart']}, and ends on {eventInfo['dateEnd']}")
-print(_events)
-print(getTeamList(eventCode="CTHAR"))
-
-
+print(responce)
