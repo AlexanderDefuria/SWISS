4056abc0d98a9d3b3cf27fc22599ce964b05f255
AlexanderDeFuria <defuria.alexander@gmail.com>
2023-02-03 17:52:17 -0500

    imports almost good to go

--- apps/entry/imports.py
+++ apps/entry/imports.py
@@ -1,68 +1,54 @@
 import requests
-import json
-import os
-
-from django.core.exceptions import ImproperlyConfigured
-
-from apps.entry.models import *
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
-
-
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
-
+from apps.entry.models import Team
+from utils import get_secret
 
 _headers = {'Authorization': f'Basic {get_secret("FIRST_API_BASE64")}'}
 _events = []
 _baseUrl = 'https://frc-api.firstinspires.org/v3.0/2023/'
-def getTeams(page = None, eventCode = None):
-    _request = f'{_baseUrl}teams'
-    if page and eventCode:
-        _request += f'?page={page}&eventCode={eventCode}'
-    elif eventCode:
-        _request += f'?eventCode={eventCode}'
+
+def get_teams(page=None, event_code=None):
+    request = f'{_baseUrl}teams'
+    if page and event_code:
+        request += f'?page={page}&eventCode={event_code}'
+    elif event_code:
+        request += f'?eventCode={event_code}'
     elif page:
-        _request += f'?page={page}'
-    else:
-        pass
-    team_count = requests.get(_request, headers=_headers).json()
+        request += f'?page={page}'
+
+    team_count = requests.get(request, headers=_headers).json()
     return team_count
 
-def getEvent():
-    event = requests.get(f'{_baseUrl}events', headers=_headers).json()
-    return event
+
+def get_all_events():
+    return requests.get(f'{_baseUrl}events', headers=_headers).json()
 
 
-baseTeamInfo = requests.get(f'{_baseUrl}teams', headers=_headers).json()
+def get_team_list(event_code=None):
+    base_team_info = requests.get(f'{_baseUrl}teams', headers=_headers).json()
+    page_total = int(base_team_info['pageTotal'])
 
-pageTotal = int(baseTeamInfo['pageTotal'])
-event = getEvent()['Events']
+    for current_page in range(1, page_total):
+        page_info = get_teams(current_page, event_code)
+        getTeamNumbers = page_info['teamCountPage']
+        for team_index in range(getTeamNumbers):
+            team_info = page_info['teams'][team_index]
+            print(f"{team_info['nameShort']} {team_info['teamNumber']} located "
+                  f"in {team_info['stateProv']}, {team_info['country']}")
+            new_team = Team()
+            new_team.number = team_info['teamNumber']
+            new_team.name = team_info['nameShort']
+            new_team.id = new_team.number
+            new_team.save()
 
-def getTeamList(eventCode = None):
-    for currentPage in range(1,pageTotal):
-        pageInfo = getTeams(currentPage,eventCode)
-        getTeamNumbers = pageInfo['teamCountPage']
-        for teamIndex in range(getTeamNumbers):
-            teamInfo = pageInfo['teams'][teamIndex]
-            print(f"{teamInfo['nameShort']} {teamInfo['teamNumber']} located in {teamInfo['stateProv']}, {teamInfo['country']}")
+def import_first():
+    event = get_all_events()['Events']
 
+    for currentEvent in range(len(event)):
+        eventInfo = event[currentEvent]
+        _events.append(eventInfo['code'])
+        print(f"{eventInfo['name']} has the first key of {eventInfo['code']}, and the district "
+              f"key of {eventInfo['districtCode']}, is the event type of {eventInfo['type']},"
+              f" starts on {eventInfo['dateStart']}, and ends on {eventInfo['dateEnd']}")
 
-
-for currentEvent in range(len(event)):
-    eventInfo = event[currentEvent]
-    _events.append(eventInfo['code'])
-    print(f"{eventInfo['name']} has the first key of {eventInfo['code']}, and the district key of {eventInfo['districtCode']}, is the event type of {eventInfo['type']}, starts on {eventInfo['dateStart']}, and ends on {eventInfo['dateEnd']}")
-print(_events)
-print(getTeamList(eventCode="CTHAR"))
-
-
+    print(_events)
+    print(get_team_list(event_code="CTHAR"))
